be9b301bbd15ce9999cdf422892651c4721c6771
Lucas Pinheiro <lucaspinheiro.almeyda@gmail.com>
2023-10-31 19:27:10 -0300

    ignit time plus context

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,40 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const LayoutContainer = styled.div `
-    max-width: 74rem;
-    height: calc(100vh - 10rem);
-    margin: 5rem auto;
-    padding: 2.5rem;
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
 
-    background: ${(props) => props.theme["gray-800"]};
-    border-radius: 8px;
-    display:flex;
-    flex-direction: column;
+  background: ${(props) => props.theme['gray-800']};
+  border-radius: 8px;
 
-    nav {
-        display: flex;
-        gap: 0.5rem;
-        
-        a{
-            width: 3rem;
-            height: 3rem;
-
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            color: ${props => props.theme["gray-100"]};
-
-            border-bottom: 3px solid transparent;
-            border-top: 3px solid transparent;
-
-            &:hover {
-                border-bottom: 3px solid ${(props) => props.theme["green-500"]};
-            }
-
-            &:active {
-               color: ${(props) => props.theme["green-500"]};
-            }
-        }
-    }
+  display: flex;
+  flex-direction: column;
 `
